c02ed494be5e8840838967e6671f082f6b0beb31
JonhNino <jonhninoa@gmail.com>
2023-04-10 21:56:16 -0500

    diciembre

---
+++ src/main/java/com/diciembre/app/DiciembreApplication.java
@@ -0,0 +1,13 @@
+package com.diciembre.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiciembreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiciembreApplication.class, args);
+	}
+
+}
